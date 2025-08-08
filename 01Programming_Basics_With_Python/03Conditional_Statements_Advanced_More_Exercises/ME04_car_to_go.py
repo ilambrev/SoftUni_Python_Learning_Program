b21fbf9aaba4f844b5a6380d440986be09eeeed0
Ivan Lambrev <ilambrev@abv.bg>
2025-08-08 12:56:55 +0300

    Car To Go

---
+++ 01Programming_Basics_With_Python/03Conditional_Statements_Advanced_More_Exercises/ME04_car_to_go.py
@@ -0,0 +1,30 @@
+budget = float(input())
+season = input()
+
+car_class = ''
+car_type = ''
+car_price = 0.0
+
+if budget <= 100:
+    car_class = 'Economy class'
+    if season == 'Summer':
+        car_type = 'Cabrio'
+        car_price = budget * 0.35
+    elif season == 'Winter':
+        car_type = 'Jeep'
+        car_price = budget * 0.65
+elif budget <= 500:
+    car_class = 'Compact class'
+    if season == 'Summer':
+        car_type = 'Cabrio'
+        car_price = budget * 0.45
+    elif season == 'Winter':
+        car_type = 'Jeep'
+        car_price = budget * 0.80
+elif budget > 500:
+    car_class = 'Luxury class'
+    car_type = 'Jeep'
+    car_price = budget * 0.9
+
+print(car_class)
+print(f'{car_type} - {car_price:.2f}')
