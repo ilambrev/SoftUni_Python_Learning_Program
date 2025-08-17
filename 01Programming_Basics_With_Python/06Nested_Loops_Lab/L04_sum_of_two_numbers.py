624e551f2f3b467db50ef72c431d484ce13f48ec
Ivan Lambrev <ilambrev@abv.bg>
2025-08-17 11:23:28 +0300

    Sum Of Two Numbers

---
+++ 01Programming_Basics_With_Python/06Nested_Loops_Lab/L04_sum_of_two_numbers.py
@@ -0,0 +1,20 @@
+interval_start = int(input())
+interval_end = int(input())
+magic_number = int(input())
+
+combination_number = 0
+is_combination_found = False
+
+for first_number in range(interval_start, interval_end + 1):
+    for second_number in range(interval_start, interval_end + 1):
+        combination_number += 1
+        
+        if first_number + second_number == magic_number:
+            print(f'Combination N:{combination_number} ({first_number} + {second_number} = {magic_number})')
+            is_combination_found = True
+            
+    if is_combination_found:
+        break
+    
+else:
+    print(f'{combination_number} combinations - neither equals {magic_number}')
