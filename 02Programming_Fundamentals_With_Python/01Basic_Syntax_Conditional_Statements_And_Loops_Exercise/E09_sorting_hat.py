df1b8fb0b5422d7211821258c88029a8d9c88112
Ivan Lambrev <ilambrev@abv.bg>
2025-09-17 09:45:09 +0300

    Sorting Hat

---
+++ 02Programming_Fundamentals_With_Python/01Basic_Syntax_Conditional_Statements_And_Loops_Exercise/E09_sorting_hat.py
@@ -0,0 +1,24 @@
+command = input()
+
+while not command == 'Welcome!':
+    name = command
+
+    if name == 'Voldemort':
+        print('You must not speak of that name!')
+        break
+
+    name_length = len(name)
+
+    if name_length < 5:
+        print(f'{name} goes to Gryffindor.')
+    elif name_length == 5:
+        print(f'{name} goes to Slytherin.')
+    elif name_length == 6:
+        print(f'{name} goes to Ravenclaw.')
+    elif name_length > 6:
+        print(f'{name} goes to Hufflepuff.')
+
+    command = input()
+
+else:
+    print('Welcome to Hogwarts.')
