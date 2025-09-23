545802a210439883db195af3d5a68c485f502466
Ivan Lambrev <ilambrev@gmail.com>
2025-09-23 10:59:03 +0300

    Centuries To Minutes

--- 02Programming_Fundamentals_With_Python/02Data_Types_And_Variables_Lab/L04_centuries_to_minutes.py
+++ 02Programming_Fundamentals_With_Python/02Data_Types_And_Variables_Lab/L04_centuries_to_minutes.py
@@ -3,8 +3,8 @@ from math import floor
 centuries = int(input())
 
 years = centuries * 100
-days = years * 365.2422
-hours = floor(days) * 24
+days = floor(years * 365.2422)
+hours = days * 24
 minutes = hours * 60
 
-print(f'{centuries} centuries = {years} years = {floor(days)} days = {hours} hours = {minutes} minutes')
+print(f'{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes')
