d4ea8231e1fa8052a69f0cbe40439fc861b686bf
Ivan Lambrev <ilambrev@gmail.com>
2025-10-02 09:16:22 +0300

    Gladiator Expenses

---
+++ 02Programming_Fundamentals_With_Python/02Data_Types_And_Variables_Exercise/E10_gladiator_expenses.py
@@ -0,0 +1,24 @@
+lost_fights_count = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+
+expenses = 0.0
+counter = 0
+
+for lost_fight in range(1, lost_fights_count + 1):
+    if lost_fight % 2 == 0:
+        expenses += helmet_price
+
+    if lost_fight % 3 == 0:
+        expenses += sword_price
+
+    if lost_fight % 2 == 0 and lost_fight % 3 == 0:
+        counter += 1
+        expenses += shield_price
+
+        if counter % 2 == 0:
+            expenses += armor_price
+
+print(f"Gladiator expenses: {expenses:.2f} aureus")
