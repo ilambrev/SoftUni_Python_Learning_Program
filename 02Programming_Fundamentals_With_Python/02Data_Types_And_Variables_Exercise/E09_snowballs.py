ef61fccb95118a9c17befdfadc88f160b970f6b2
Ivan Lambrev <ilambrev@gmail.com>
2025-10-02 09:09:54 +0300

    Snowballs

---
+++ 02Programming_Fundamentals_With_Python/02Data_Types_And_Variables_Exercise/E09_snowballs.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+best_snowball_weight = 0
+best_snowball_time = 0
+best_snowball_quality = 0
+best_snowball_value = 0
+
+for i in range(n):
+    snowball_weight = int(input())
+    snowball_time = int(input())
+    snowball_quality = int(input())
+
+    snowball_value = int((snowball_weight / snowball_time) ** snowball_quality)
+
+    if snowball_value > best_snowball_value:
+        best_snowball_weight = snowball_weight
+        best_snowball_time = snowball_time
+        best_snowball_quality = snowball_quality
+        best_snowball_value = snowball_value
+
+print(f"{best_snowball_weight} : {best_snowball_time} = {best_snowball_value} ({best_snowball_quality})")
