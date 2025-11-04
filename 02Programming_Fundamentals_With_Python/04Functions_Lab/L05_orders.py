2f773e91894666d195cb1d583ebbb3c82659f3f6
Ivan Lambrev <ilambrev@gmail.com>
2025-11-04 10:46:11 +0200

    Orders

---
+++ 02Programming_Fundamentals_With_Python/04Functions_Lab/L05_orders.py
@@ -0,0 +1,14 @@
+def calculate_total_price(product, quantity):
+    if product == "coffee":
+        return quantity * 1.50
+    elif product == "water":
+        return quantity * 1.00
+    elif product == "coke":
+        return quantity * 1.40
+    elif product == "snacks":
+        return quantity * 2.00
+
+product = input()
+quantity = int(input())
+
+print(f"{calculate_total_price(product, quantity):.2f}")
